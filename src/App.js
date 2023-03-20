da18e531c3b20dc76d0d9b9ce665f8fb7b2bdade
P1ST-0N <luckylionia@gmail.com>
2023-03-21 00:10:33 +0200

    test demo

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Container from "./components/Container/Container";
+import Section from "./components/Section/Section";
+import Statistics from "./components/Statistics/Statistics";
+import FeedbackOptions from "./components/Feedback/FeedbackOptions";
+import Notification from "./components/Notification/Notification";
+
+const App = () => {
+  const [stats, setStats] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const {good, neutral, bad} = stats;
+
+  const handleIncrementStat = ({target: {textContent} }) => 
+  setStats((prState) => ({
+    ...prState,
+    [textContent]: stats[textContent] + 1,
+  }));
+
+const countTotalFeedback = () => {
+const result = good + neutral + bad;
+return result;
+};
+
+const countPositiveFeedbackPercentage = () =>
+Math.round(((good * 100) / countTotalFeedback()) * 10) / 10;
+
+  return (
+    <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+        options={Object.keys(stats)}
+        onLeaveFeedback={handleIncrementStat}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {countTotalFeedback() ? (
+          <Statistics 
+      good={good} 
+      neutral={neutral} 
+      bad={bad} 
+      total={countTotalFeedback} 
+      positivePercentage={countPositiveFeedbackPercentage}
+      />
+        ) : (
+<Notification message="No feedback given"></Notification>
+        )}
+     </Section>
+    </Container>
+  );
+};
+
+export default App;
